f928bebd320cbb2a0dacce9ac690eee2e093ce1f
solomon-igbigbi <solomonigbigbi07@gmail.com>
2022-03-02 15:43:06 +0100

    fixed paystack payment issue and ratings issue

--- src/prisma/seed/Plan/planData.js
+++ src/prisma/seed/Plan/planData.js
@@ -1,25 +1,25 @@
 const plan = [
-    {
-        "name": "Yabure Monthly",
-        "interval": "monthly",
-        "amount": "200000",
-        "view_amount": "2000",
-        "plan_code": "PLN_zoulmka0p15x7m7"
-    },
-    {
-        "name": "Yabure Quarterly",
-        "interval": "quarterly",
-        "amount": "500000",
-        "view_amount": "5000",
-        "plan_code": "PLN_2a1yyljo0qrn97h"
-    },
-    {
-        "name": "Yabure Annually",
-        "interval": "annually",
-        "view_amount": "15000",
-        "amount": "1500000",
-        "plan_code": "PLN_v0zg1qnwtnd81aj"
-    },
-]
+  {
+    name: "Yabure Monthly",
+    interval: "monthly",
+    amount: "200000",
+    view_amount: "2000",
+    plan_code: "PLN_14t1xyc2cp01vu6",
+  },
+  {
+    name: "Yabure Quarterly",
+    interval: "quarterly",
+    amount: "500000",
+    view_amount: "5000",
+    plan_code: "PLN_qmpmpw3zrjdmx1l",
+  },
+  {
+    name: "Yabure Annually",
+    interval: "annually",
+    view_amount: "15000",
+    amount: "1500000",
+    plan_code: "PLN_8wmnxuacpng7nfl",
+  },
+];
 
-module.exports = plan
+module.exports = plan;
