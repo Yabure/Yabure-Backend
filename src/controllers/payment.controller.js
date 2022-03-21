a80c8dca3130068538e7178a92cb69887660ed55
solomon-igbigbi <solomonigbigbi07@gmail.com>
2022-03-21 09:02:17 +0100

    added payment endpoint

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,31 +1,49 @@
+const {
+  getAllSubscription,
+  initializeTransaction,
+  addpayment,
+} = require("../services/payment.service");
+const Response = require("../utils/errorResponse");
+const validateErrorFormatter = require("../utils/validateErrorFormatter");
 
-const { getAllSubscription, initializeTransaction, subscriptionWebhook } = require("../services/payment.service")
-const Response = require("../utils/errorResponse")
-const validateErrorFormatter = require("../utils/validateErrorFormatter")
-
-
-const paymentController = {}
+const paymentController = {};
 
 paymentController.getAll = async (req, res) => {
-    try{
-        const data = await getAllSubscription()
-        return Response.SUCCESS({ response: res, data})
-    } catch(err) {
-       const errors = await validateErrorFormatter(err) 
-       return Response.INVALID_REQUEST({ response: res, errors})
-    }
-}
+  try {
+    const data = await getAllSubscription();
+    return Response.SUCCESS({ response: res, data });
+  } catch (err) {
+    const errors = await validateErrorFormatter(err);
+    return Response.INVALID_REQUEST({ response: res, errors });
+  }
+};
 
 paymentController.initializeTransaction = async (req, res) => {
-    try{
-        const data = await initializeTransaction(req.body)
-        return Response.SUCCESS({ response: res, data})
-    } catch(err) {
-       const errors = await validateErrorFormatter(err) 
-       return Response.INVALID_REQUEST({ response: res, errors})
-    }
-}
+  try {
+    const data = await initializeTransaction(req.body);
+    return Response.SUCCESS({ response: res, data });
+  } catch (err) {
+    const errors = await validateErrorFormatter(err);
+    return Response.INVALID_REQUEST({ response: res, errors });
+  }
+};
 
+paymentController.addPayment = async (req, res) => {
+  try {
+    await addpayment(req);
+    return Response.SUCCESS({
+      response: res,
+      message: "Request successful",
+      data: { subscribed: true },
+    });
+  } catch (err) {
+    const errors = await validateErrorFormatter(err);
+    return Response.INVALID_REQUEST({
+      response: res,
+      errors,
+      subscribed: false,
+    });
+  }
+};
 
-
-module.exports = paymentController
+module.exports = paymentController;
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,9 +1,10 @@
 const paymentController = require("../controllers/payment.controller");
 
 const payment = (fastify, options, next) => {
-    fastify.get("/", paymentController.getAll);
-    fastify.post("/activate", paymentController.initializeTransaction);
-    next();
-}
+  fastify.get("/", paymentController.getAll);
+  fastify.post("/activate", paymentController.initializeTransaction);
+  fastify.post("/save", paymentController.addPayment);
+  next();
+};
 
-module.exports = payment
+module.exports = payment;
