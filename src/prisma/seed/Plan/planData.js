699fd430da24d564f97e23a79750d265810dd99b
solomon-igbigbi <solomonigbigbi07@gmail.com>
2022-04-21 10:58:08 +0100

    moved to live

--- src/prisma/seed/Plan/planData.js
+++ src/prisma/seed/Plan/planData.js
@@ -4,21 +4,21 @@ const plan = [
     interval: "monthly",
     amount: "200000",
     view_amount: "2000",
-    plan_code: "PLN_14t1xyc2cp01vu6",
+    plan_code: "PLN_0gjnnmfyqjlogic",
   },
   {
     name: "Yabure Quarterly",
     interval: "quarterly",
     amount: "500000",
     view_amount: "5000",
-    plan_code: "PLN_qmpmpw3zrjdmx1l",
+    plan_code: "PLN_q1zsao0a8p9aesn",
   },
   {
     name: "Yabure Annually",
     interval: "annually",
     view_amount: "15000",
     amount: "1500000",
-    plan_code: "PLN_8wmnxuacpng7nfl",
+    plan_code: "PLN_smoloa85ydvx3tp",
   },
 ];
 
