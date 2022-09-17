922a21dfd5d53cdacee95d8245124edfe4ba7b9c
Solomon Igbigbi <solomonigbigbi07@gmail.com>
2022-09-18 00:59:37 +0100

    fixing

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const App = require("./src/app");
 
 App(server);
 
-server.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT, "0.0.0.0", () => {
   console.log("listening on port " + process.env.PORT);
 });
