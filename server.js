d6bd6522e50c48d94430df274ebc0b0f1c7d43f5
solomon igbigbi <solomonigbigbi07@gmail.com>
2025-05-25 12:52:52 +0100

    fixed port

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const App = require("./src/app");
 
 App(server);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-server.listen({ port: PORT }, (err) => {
+server.listen({ port: PORT, host: "0.0.0.0" }, (err) => {
   if (err) {
     console.error("Error starting server:", err);
     process.exit(1);
